20d068f3a6e42b1bb6e7d3121320bb11ea25a6f5
souvik <souvikghosh734@gmail.com>
2024-07-31 09:50:14 +0530

    removed lws

--- src/components/AddTaskNav.jsx
+++ src/components/AddTaskNav.jsx
@@ -1,5 +1,4 @@
- import logo from "../assets/lws-logo-en.svg";
- 
+  
  
  export default function AddTaskNavbar()  
  {
@@ -7,7 +6,7 @@
  <div className="container mx-auto flex items-center justify-between gap-x-6">
    
        <a href="/">
-           <img className="h-[45px]" src={logo} alt="Lws" />
+        <div className="text-3xl ">TASKER</div>
        </a>  
 
    <form>
